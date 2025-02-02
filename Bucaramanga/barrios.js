6036eb67f53ae27c4ee7e96f80e261fb9771ac53
nbarinas <46245234+nbarinas@users.noreply.github.com>
2025-02-02 14:03:41 -0500

    Barrios de Bucaramanga

--- Bucaramanga/barrios.js
+++ Bucaramanga/barrios.js
@@ -1,5 +1,39 @@
 export const barrios = [
 
-{ localidad: "Cañaveralejo", nombre: "Vipasa", estrato: 5, sector: "Norte" },
-{ localidad: "Cali-Aguacatal", nombre: "Yumbo Guabinas", estrato: 2, sector: "Norte" }
+    // Bucaramanga
+    { localidad: "Bucaramanga", nombre: "Cabecera del Llano", estrato: 5, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "Provenza", estrato: 4, sector: "Sur" },
+    { localidad: "Bucaramanga", nombre: "San Francisco", estrato: 3, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "Gaitán", estrato: 3, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "Antonia Santos", estrato: 3, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "Álvarez", estrato: 4, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "San Alonso", estrato: 4, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "La Joya", estrato: 2, sector: "Sur" },
+    { localidad: "Bucaramanga", nombre: "La Concordia", estrato: 3, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "Comuneros", estrato: 3, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "San Miguel", estrato: 3, sector: "Centro" },
+    { localidad: "Bucaramanga", nombre: "Quinta Brigada", estrato: 3, sector: "Norte" },
+    { localidad: "Bucaramanga", nombre: "La Pedregosa", estrato: 2, sector: "Norte" },
+
+    // Floridablanca
+    { localidad: "Floridablanca", nombre: "Cañaveral", estrato: 5, sector: "Norte" },
+    { localidad: "Floridablanca", nombre: "El Bosque", estrato: 4, sector: "Centro" },
+    { localidad: "Floridablanca", nombre: "La Castellana", estrato: 4, sector: "Centro" },
+    { localidad: "Floridablanca", nombre: "Lagos", estrato: 3, sector: "Sur" },
+    { localidad: "Floridablanca", nombre: "Caracolí", estrato: 3, sector: "Centro" },
+    { localidad: "Floridablanca", nombre: "Zapamanga", estrato: 3, sector: "Sur" },
+
+    // Girón
+    { localidad: "Girón", nombre: "El Poblado", estrato: 3, sector: "Norte" },
+    { localidad: "Girón", nombre: "Bellavista", estrato: 3, sector: "Centro" },
+    { localidad: "Girón", nombre: "Arenales", estrato: 2, sector: "Sur" },
+    { localidad: "Girón", nombre: "Villa Campestre", estrato: 3, sector: "Centro" },
+
+    // Piedecuesta
+    { localidad: "Piedecuesta", nombre: "San Cristóbal", estrato: 3, sector: "Centro" },
+    { localidad: "Piedecuesta", nombre: "Villa de San Carlos", estrato: 3, sector: "Norte" },
+    { localidad: "Piedecuesta", nombre: "Barro Blanco", estrato: 2, sector: "Sur" },
+    { localidad: "Piedecuesta", nombre: "Campo Verde", estrato: 3, sector: "Centro" },
+    { localidad: "Piedecuesta", nombre: "Prado Alto", estrato: 4, sector: "Norte" }
+
 ];
